3d74202ebbebd544b20357a33f31f2d1c635b2a3
elaaasri <anas_elasri@outlook.fr>
2023-07-17 13:51:07 +0100

    adds choose mark options and tie it to the DOM, sets data to the the game board

--- script.js
+++ script.js
@@ -1,45 +1,120 @@
-let gameBoardModule = (function () {
-  const allSquares = document.querySelectorAll(".square");
-
-  function playerFactory(name, mark) {
-    return {
-      name,
-      mark,
-    };
+const gameBoard = (function () {
+  const board = ["X", "O"];
+  const rows = 3;
+  const columns = 3;
+  const the2Dboard = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+  ];
+  for (let i = 0; i < the2Dboard.length; i++) {
+    for (let j = 0; j < the2Dboard[i].length; j++) {
+      // console.log(the2Dboard[i]);
+    }
   }
-  const playerOne = playerFactory("player one", "X");
-  const playerTwo = playerFactory("player two", "O");
-
-  const board = [playerOne.mark, playerTwo.mark];
-
-  function playerOneRound() {
-    return board[0];
-  }
-  function playerTwoRound() {
-    return board[1];
-  }
-
-  let clickCount = 0;
-  function addMarksToSpecificSpots() {
-    allSquares.forEach((square) =>
-      square.addEventListener("click", function () {
-        // stops players from playing in spots that already taken :
-        if (square.textContent != "") return;
-        // display the mark on the specific spot :
-        if (clickCount === 0) {
-          square.textContent = playerOneRound();
-          clickCount++;
-        } else if (clickCount === 1) {
-          square.textContent = playerTwoRound();
-          clickCount = 0;
-        }
-        console.log(board);
-      })
-    );
-  }
-
-  return {
-    addMarksToSpecificSpots,
-  };
+  return { the2Dboard, board };
 })();
-gameBoardModule.addMarksToSpecificSpots();
+
+// players factory for creating new player :
+function playersFactory(name, mark) {
+  const getName = function () {
+    return name;
+  };
+  const getMark = function () {
+    return mark;
+  };
+  return { getName, getMark };
+}
+const playerOne = playersFactory("Player One", "X");
+const playerTwo = playersFactory("Player Two", "O");
+
+function setDataBoard() {
+  const dataBoard = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+  ];
+  for (let i = 0; i < dataBoard.length; i++) {
+    for (let j = 0; j < dataBoard[i].length; j++) {
+      const button = document.createElement("button");
+      const boardContainer = document.querySelector(".board");
+      boardContainer.appendChild(button);
+      button.setAttribute("data-value", dataBoard[i][j]);
+    }
+  }
+}
+setDataBoard();
+
+// function checksForTheWinner() {
+//   const allButtons = document.querySelectorAll("button");
+//   allButtons.forEach((button) => {
+//     button.addEventListener("click", function () {
+//       const buttonDataValue = button.getAttribute("data-value");
+//       console.log(buttonDataValue);
+//     });
+//   });
+// }
+// checksForTheWinner();
+
+function addMarksToSpecificSpot() {
+  const allButtons = document.querySelectorAll("button");
+  let clickCount = 0;
+
+  allButtons.forEach((button) =>
+    button.addEventListener("click", function () {
+      // stops players from playing in spots that already taken :
+      if (button.textContent != "") return;
+      // display the mark on the specific spot :
+      if (clickCount === 0) {
+        button.textContent = addChoosenMark();
+        clickCount++;
+      } else if (clickCount === 1) {
+        button.textContent = getOtherMark();
+        clickCount = 0;
+      }
+    })
+  );
+}
+addMarksToSpecificSpot();
+
+// func to add the choosen mark :
+const playerOptionSelect = document.getElementById("optionSelect");
+function addChoosenMark() {
+  const selectedOption = playerOptionSelect.value;
+  return selectedOption === "Player One"
+    ? playerOne.getMark()
+    : selectedOption === "Player Two"
+    ? playerTwo.getMark()
+    : "";
+}
+playerOptionSelect.addEventListener("click", addChoosenMark);
+
+// func to get the other player mark :
+function getOtherMark() {
+  return addChoosenMark() === playerOne.getMark()
+    ? playerTwo.getMark()
+    : addChoosenMark() === playerTwo.getMark()
+    ? playerOne.getMark()
+    : "";
+}
+
+// function render() {
+//   const allbuttons = document.querySelectorAll(".button");
+//   let clickCount = 0;
+
+//   allbuttons.forEach((button) =>
+//     button.addEventListener("click", function () {
+//       // stops players from playing in spots that already taken :
+//       if (button.textContent != "") return;
+//       // display the mark on the specific spot :
+//       if (clickCount === 0) {
+//         button.textContent = addChoosenMark();
+//         clickCount++;
+//       } else if (clickCount === 1) {
+//         button.textContent = getOtherMark();
+//         clickCount = 0;
+//       }
+//     })
+//   );
+// }
+// render();
