63cbdb71505cabc4294c8fef4ad32b73ef3ac5b4
elaaasri <anas_elasri@outlook.fr>
2023-07-24 23:58:36 +0100

    fix tie condition, adds landing page

--- landing.js
+++ landing.js
@@ -1,1 +1,9 @@
-console.log("zbe");
+const startGameButton = document.getElementById("test");
+console.log(startGameButton);
+console.log("zbeaaaaaaaa");
+
+function getplayersName() {
+  console.log("zbe");
+}
+
+startGameButton.addEventListener("click", getplayersName);
